7e0e62415f2ead0bcda8bdb441ced3810ffcbdba
James Priest <james-priest@outlook.com>
2019-01-18 10:48:35 -0800

    refactor: Class components to functional components

--- src/App.js
+++ src/App.js
@@ -4,23 +4,21 @@ import { profiles, users, movies } from './data.js';
 import PopularMovies from './PopularMovies';
 import MovieCardsList from './MovieCardsList';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src="logo.svg" className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-          <p>Exercise - Functional Component</p>
-        </header>
-        <main className="App-main">
-          <h2>How Popular is Your Favorite Movie?</h2>
-          <PopularMovies profiles={profiles} users={users} movies={movies} />
-          <MovieCardsList profiles={profiles} movies={movies} users={users} />
-        </main>
-      </div>
-    );
-  }
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="logo.svg" className="App-logo" alt="logo" />
+        <h1 className="App-title">ReactND - Coding Practice</h1>
+        <p>Exercise - Functional Component</p>
+      </header>
+      <main className="App-main">
+        <h2>How Popular is Your Favorite Movie?</h2>
+        <PopularMovies profiles={profiles} users={users} movies={movies} />
+        <MovieCardsList profiles={profiles} movies={movies} users={users} />
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/MovieCardsList.js
+++ src/MovieCardsList.js
@@ -1,47 +1,45 @@
 import React, { Component } from 'react';
 import MovieCard from './MovieCard';
 
-class MovieCardsList extends Component {
-  render() {
-    const { profiles, users, movies } = this.props;
-    const usersByMovie = {}; // create empty object
+function MovieCardsList(props) {
+  const { profiles, users, movies } = props;
+  const usersByMovie = {}; // create empty object
 
-    profiles.forEach(profile => {
-      // loop thru ea. profile item
-      const movieID = profile.favoriteMovieID; // get movieID as key
-      console.log('movieID:', movieID);
+  profiles.forEach(profile => {
+    // loop thru ea. profile item
+    const movieID = profile.favoriteMovieID; // get movieID as key
+    console.log('movieID:', movieID);
 
-      if (usersByMovie[movieID]) {
-        // if this movie key exists
-        usersByMovie[movieID].push(profile.userID); // push user onto array
-      } else {
-        // else if movie key does not exit
-        usersByMovie[movieID] = [profile.userID]; // assign user array to key
-      }
-      console.log('usersByMovie', usersByMovie);
-    });
+    if (usersByMovie[movieID]) {
+      // if this movie key exists
+      usersByMovie[movieID].push(profile.userID); // push user onto array
+    } else {
+      // else if movie key does not exit
+      usersByMovie[movieID] = [profile.userID]; // assign user array to key
+    }
+    console.log('usersByMovie', usersByMovie);
+  });
 
-    console.log('Object.keys(movies):', Object.keys(movies));
+  console.log('Object.keys(movies):', Object.keys(movies));
 
-    const movieCards = Object.keys(movies).map(mID => {
-      console.log('------------------ movieCard');
-      console.log('id', mID);
-      console.log('users', users);
-      console.log('usersByMovie[mID]', usersByMovie[mID]);
-      console.log('movies[mID]', movies[mID]);
+  const movieCards = Object.keys(movies).map(mID => {
+    console.log('------------------ movieCard');
+    console.log('id', mID);
+    console.log('users', users);
+    console.log('usersByMovie[mID]', usersByMovie[mID]);
+    console.log('movies[mID]', movies[mID]);
 
-      return (
-        <MovieCard
-          key={mID}
-          users={users}
-          usersWhoLikedMovie={usersByMovie[mID]}
-          movieInfo={movies[mID]}
-        />
-      );
-    });
+    return (
+      <MovieCard
+        key={mID}
+        users={users}
+        usersWhoLikedMovie={usersByMovie[mID]}
+        movieInfo={movies[mID]}
+      />
+    );
+  });
 
-    return <div className="PopularMovies-container">{movieCards}</div>;
-  }
+  return <div className="PopularMovies-container">{movieCards}</div>;
 }
 
 export default MovieCardsList;
